3494074592df8a71eb46cbccc32a6717e96e8859
Amidope <66752876+Amidope@users.noreply.github.com>
2025-10-05 13:04:28 +0500

    add sync service

---
+++ app/Console/Commands/SyncStocks.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Services\SyncService;
+use Illuminate\Console\Command;
+
+class SyncStocks extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sync:stocks';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Sync stocks with API';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct(private SyncService $syncService)
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle(): int
+    {
+        $this->syncService->sync();
+        return self::SUCCESS;
+    }
+}
---
+++ app/Providers/SyncServiceProvider.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Providers;
+
+use App\Console\Commands\SyncIncomes;
+use App\Console\Commands\SyncOrders;
+use App\Console\Commands\SyncSales;
+use App\Console\Commands\SyncStocks;
+use App\Models\Income;
+use App\Models\Order;
+use App\Models\Sale;
+use App\Models\Stock;
+use App\Services\ApiClient;
+use App\Services\SyncService;
+use Illuminate\Support\ServiceProvider;
+
+class SyncServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton(SyncOrders::class, fn($app) =>
+        new SyncOrders(
+            new SyncService(
+                $app->make(ApiClient::class),
+                endpoint: 'orders',
+                modelClass: Order::class,
+                fromDate: '2004-01-01',
+                toDate: now()->format('Y-m-d')
+            )
+        )
+        );
+
+        $this->app->singleton(SyncSales::class, fn($app) =>
+        new SyncSales(
+            new SyncService(
+                $app->make(ApiClient::class),
+                endpoint: 'sales',
+                modelClass: Sale::class,
+                fromDate: '2004-01-01',
+                toDate: now()->format('Y-m-d')
+            )
+        )
+        );
+
+        $this->app->singleton(SyncIncomes::class, fn($app) =>
+        new SyncIncomes(
+            new SyncService(
+                $app->make(ApiClient::class),
+                endpoint: 'incomes',
+                modelClass: Income::class,
+                fromDate: '2004-01-01',
+                toDate: now()->format('Y-m-d')
+            )
+        )
+        );
+
+        $this->app->singleton(SyncStocks::class, fn($app) =>
+        new SyncStocks(
+            new SyncService(
+                $app->make(ApiClient::class),
+                endpoint: 'stocks',
+                modelClass: Stock::class,
+                fromDate: now()->format('Y-m-d'),
+                toDate: null
+            )
+        )
+        );
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
